c4a46529de020d8ec62d7f1b6196a1e5065aa98a
Garrett Grimm <grimmdude@gmail.com>
2022-02-16 21:34:47 -0800

    Support expanding recurring events.

--- src/Facade/Requests/CalendarQueryFilter.php
+++ src/Facade/Requests/CalendarQueryFilter.php
@@ -45,13 +45,15 @@ final class CalendarQueryFilter
      * @param bool $get_calendar_data
      * @param DateTime $from
      * @param DateTime $to
+     * @param bool $expand
      */
-    public function __construct($get_etags = true, $get_calendar_data = false, DateTime $from = null,  DateTime $to = null)
+    public function __construct($get_etags = true, $get_calendar_data = false, DateTime $from = null,  DateTime $to = null, bool $expand = false)
     {
         $this->get_etags         = $get_etags;
         $this->get_calendar_data = $get_calendar_data;
         $this->from              = $from;
         $this->to                = $to;
+        $this->expand            = $expand;
 
         if(!is_null($this->from) && !is_null($this->to) && $this->from > $this->to)
             throw new \InvalidArgumentException("from should be lower than to param");
@@ -87,6 +89,15 @@ final class CalendarQueryFilter
     public function getFrom()
     {
         return $this->from;
+    }
+
+
+    /**
+     * @return Boolean
+     */
+    public function getExpand()
+    {
+        return $this->expand;
     }
 
 }
